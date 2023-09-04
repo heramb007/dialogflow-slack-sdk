b58e297e98cb786ce887b67bf89fec73de1140bd
heramb007 <50517960+heramb007@users.noreply.github.com>
2023-09-04 11:48:52 +0530

    Update token.go

--- functions/config/token.go
+++ functions/config/token.go
@@ -2,7 +2,7 @@ package config
 
 const (
 	BOT_TOKEN = "xoxb-5845993184340-5829047534279-NX1pKTMepMiKDLhU5ZIBtCVT" // required for events api callback events
-	APP_TOKEN = "xapp-1-A05QP0V6SKX-5839371371029-5ccfe0e1b135ef4e2f752ad148007ab0fbfb84555428d8987949dd0231cb53a9" // required for socket mode
+	APP_TOKEN = "xapp-1-A05QLSZFB5L-5836953999030-c783e22c2dc795b48e9fa07f06d2f16556c3548101f892f43c30f0f83d32ae82" // required for socket mode
 
 	// if below line is uncommented, dialogflowcx,json must be present at the location relative to root
 	//CREDENTIALS_PATH     = "/functions/config/dialogflowcx.json"
